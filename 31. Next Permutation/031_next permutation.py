b6ab7e2b4730227e3fc0965a4b16c1da66d642f0
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2020-07-10 19:35:18 +0800

    Update 031_next permutation.py

--- 31. Next Permutation/031_next permutation.py
+++ 31. Next Permutation/031_next permutation.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        k, l = len(nums) -2 , len(nums) - 1 
+        while k >= 0 and nums[k] >= nums[k+1]: 
+            k -= 1 
+        if k == -1: 
+            return nums.reverse() 
+        while nums[k] >= nums[l]:  
+            l -= 1  
+        nums[k], nums[l] = nums[l], nums[k]  
+        nums[k+1:]=reversed(nums[k+1:])  
