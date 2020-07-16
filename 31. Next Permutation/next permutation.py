c5a31e22f12290e39bae9bcd74077068efff0293
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2020-07-16 23:20:04 +0800

    Update next permutation.py

--- 31. Next Permutation/next permutation.py
+++ 31. Next Permutation/next permutation.py
@@ -3,8 +3,8 @@ class Solution:
         k, l = len(nums) -2 , len(nums) - 1 
         while k >= 0 and nums[k] >= nums[k+1]: 
             k -= 1 
-        if k == -1: 
-            return nums.reverse() 
+            if k == -1: 
+                return nums.reverse() 
         while nums[k] >= nums[l]:  
             l -= 1  
         nums[k], nums[l] = nums[l], nums[k]  
