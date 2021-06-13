63fa0a5948d6ce9bf91b6c47c47206811736b8a6
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-06-14 00:47:44 +0800

    Update 3Sum Closest.py

--- 16. 3Sum Closest/3Sum Closest.py
+++ 16. 3Sum Closest/3Sum Closest.py
@@ -9,5 +9,23 @@ Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
 """
 
-class Solution:
+
+
+
+
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        diff = float('inf')
+        nums.sort()
+        for i in range(len(nums)):
+            lo, hi = i+1, len(nums) - 1
+            while (lo<hi):
+                sum = nums[i] + nums[lo] + nums[hi]
+                if abs(target - sum) < abs(diff):
+                    diff = target - sum
+                if sum < target:
+                    lo +=1
+                else:
+                    hi -=1
+            if diff == 0:
+                break
+        return target - diff
