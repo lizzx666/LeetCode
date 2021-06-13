64c2de46da1bf999679ba0b0d129be476fe94e76
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-06-14 00:13:23 +0800

    Update 3Sum Closest.py

--- 16. 3Sum Closest/3Sum Closest.py
+++ 16. 3Sum Closest/3Sum Closest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. 
 Return the sum of the three integers. You may assume that each input would have exactly one solution.
 
@@ -7,4 +7,4 @@ Input: nums = [-1,2,1,-4], target = 1
 Output: 2
 Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
-'''
+"""
