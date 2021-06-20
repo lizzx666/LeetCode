5aa06d5422f8d66ff35efe438ddacd35ff12fbe5
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-06-20 21:35:21 +0800

    Update Find the Duplicate Number.py

--- 287. Find the Duplicate Number/Find the Duplicate Number.py
+++ 287. Find the Duplicate Number/Find the Duplicate Number.py
@@ -27,3 +27,10 @@ Input: nums = [1,1,2]
 Output: 1
 
 """
+
+
+def findDuplicate(self, nums: List[int]) -> int:
+ nums.sort()
+ for i in range(len(nums)):
+  if nums[i]==nums[i+1]:
+   return nums[i]
