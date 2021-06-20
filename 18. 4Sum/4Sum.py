706089281f3e0d8ad09d530fb274d5053e143045
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-06-20 20:05:58 +0800

    Update 4Sum.py

--- 18. 4Sum/4Sum.py
+++ 18. 4Sum/4Sum.py
@@ -20,3 +20,33 @@ Input: nums = [2,2,2,2,2], target = 8
 Output: [[2,2,2,2]]
 
 """
+
+
+
+
+def fourSum(nums, target):
+      
+    def KSum(nums, target, k, result, results):
+        if len(nums)<k or k<2 or target < nums[0]*k or target > nums[-1]*k:
+            return
+        if k==2:
+            l,r = 0, len(nums)-1
+            while l<r:
+                s = nums[l] + nums[r]
+                if s==target:
+                    results.append(result + [nums[l],nums[r]])
+                    l += 1
+                    while l<r and nums[l]==nums[l-1]:
+                        l += 1
+                elif s<target:
+                    l += 1
+                else:
+                    r -= 1
+        else:
+            for i in range(len(nums)-k+1):
+                if i==0 or (i>0 and nums[i-1] != nums[i]):
+                    KSum(nums[i+1:], target-nums[i],k-1,result+[nums[i]], results)
+                    
+    results = []
+    KSum(sorted(nums),target,4,[],results)
+    return results
