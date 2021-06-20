ac8e30db6c084cd3e75dbf9b90220ca90e0ff397
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-06-20 21:42:05 +0800

    Update Find the Duplicate Number.py

--- 287. Find the Duplicate Number/Find the Duplicate Number.py
+++ 287. Find the Duplicate Number/Find the Duplicate Number.py
@@ -41,7 +41,7 @@ def findDuplicate(self, nums: List[int]) -> int:
 #method 2:
 def findDuplicate(self, nums: List[int]) -> int:
  unique = set()
- for i in range(len(nums)):
-  if nums[i] in unique:
-   return nums[i]
-  unique.add(nums[i])
+ for num in nums:
+  if num in unique:
+   return num
+  unique.add(num)
