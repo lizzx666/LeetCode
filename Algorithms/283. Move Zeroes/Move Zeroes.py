10d443a9afa9934ded6c1185c017827fe39f881d
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-04 02:27:45 +0800

    Update Move Zeroes.py

--- Algorithms/283. Move Zeroes/Move Zeroes.py
+++ Algorithms/283. Move Zeroes/Move Zeroes.py
@@ -15,3 +15,17 @@ Output: [0]
 
 
 '''
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[n]=nums[i]
+                n+=1
+        for i in range(n,len(nums)):
+            nums[i]=0
