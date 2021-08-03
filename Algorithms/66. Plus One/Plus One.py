7fd8f4e719815bdcbdfdc2769e79db1877ad941d
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-04 00:53:13 +0800

    Update Plus One.py

--- Algorithms/66. Plus One/Plus One.py
+++ Algorithms/66. Plus One/Plus One.py
@@ -22,3 +22,15 @@ Example 3:
 Input: digits = [0]
 Output: [1]
 '''
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]==9:
+                digits[i]=0
+            else:
+                digits[i] += 1
+                return digits
+
+        return [1] + digits
