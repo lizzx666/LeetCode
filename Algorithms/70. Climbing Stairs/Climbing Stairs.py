c8dc85399463913ab2827b235b8273b5cd7799ed
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-07 20:41:09 +0800

    Update Climbing Stairs.py

--- Algorithms/70. Climbing Stairs/Climbing Stairs.py
+++ Algorithms/70. Climbing Stairs/Climbing Stairs.py
@@ -19,3 +19,17 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 '''
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        else:
+            dp =[0]*(n+1)
+            dp[0] = 0
+            dp[1] = 1
+            dp[2] = 2
+            for i in range(3,n+1):
+                dp[i] = dp[i-1] + dp[i-2]
+            return dp[n]
