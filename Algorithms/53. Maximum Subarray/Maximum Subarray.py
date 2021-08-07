ec934f1da75eb65fddc937e1163e1bc5a8b2ca58
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-07 21:35:09 +0800

    Update Maximum Subarray.py

--- Algorithms/53. Maximum Subarray/Maximum Subarray.py
+++ Algorithms/53. Maximum Subarray/Maximum Subarray.py
@@ -29,3 +29,13 @@ class Solution:
                 current_subarray += nums[j]
                 max_subarray = max(max_subarray,current_subarray)
         return max_subarray
+       
+       
+#DP
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_subarray = max_subarray = nums[0]
+        for num in nums[1:]:
+            current_subarray = max(current_subarray+num,num)
+            max_subarray = max(current_subarray,max_subarray)
+
+        return max_subarray
