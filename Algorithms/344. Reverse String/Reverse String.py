094456f41fc865e04f0d5b92a08d175594ef3796
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-08 16:03:55 +0800

    Update Reverse String.py

--- Algorithms/344. Reverse String/Reverse String.py
+++ Algorithms/344. Reverse String/Reverse String.py
@@ -12,3 +12,17 @@ Output: ["h","a","n","n","a","H"]
 
 
 '''
+
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        head = 0
+        tail = len(s)-1
+        while head <= tail:
+            s[head], s[tail] = s[tail], s[head]
+            head += 1
+            tail -= 1
