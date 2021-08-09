331176cb4c2e2f21adde7e48e8253d0a580daf47
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-10 01:18:06 +0800

    Update House Robber III.py

--- Algorithms/337. House Robber III/House Robber III.py
+++ Algorithms/337. House Robber III/House Robber III.py
@@ -19,3 +19,16 @@ Output: 9
 Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
 
 '''
+
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def helper(node):
+            #return [rob this node, not rob this node]
+            if not node:
+                return (0,0)
+            left = helper(node.left)
+            right = helper(node.right)
+            rob = node.val + left[1] + right[1]
+            not_rob = max(left) + max(right)
+            return [rob, not_rob]
+        return max(helper(root))
