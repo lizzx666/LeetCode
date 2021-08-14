9bb7693dc6f741ef1175bd48c31b1ee59effde12
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-14 23:12:13 +0800

    Update Longest Increasing Subsequence.py

--- Algorithms/300. Longest Increasing Subsequence/Longest Increasing Subsequence.py
+++ Algorithms/300. Longest Increasing Subsequence/Longest Increasing Subsequence.py
@@ -20,3 +20,13 @@ Input: nums = [7,7,7,7,7,7,7]
 Output: 1
 
 '''
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1]*len(nums)
+        for i in range(1,len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i],dp[j]+1)
+        return max(dp)
