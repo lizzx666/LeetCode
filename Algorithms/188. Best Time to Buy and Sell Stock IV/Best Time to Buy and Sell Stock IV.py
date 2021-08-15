da328994b5defa87f1b443b0cc8240f7cbe46877
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-16 00:14:09 +0800

    Update Best Time to Buy and Sell Stock IV.py

--- Algorithms/188. Best Time to Buy and Sell Stock IV/Best Time to Buy and Sell Stock IV.py
+++ Algorithms/188. Best Time to Buy and Sell Stock IV/Best Time to Buy and Sell Stock IV.py
@@ -18,3 +18,24 @@ Input: k = 2, prices = [3,2,6,5,0,3]
 Output: 7
 Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
 '''
+
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        profit = 0
+        if not prices or k==0:
+            return 0
+        elif 2*k>n:
+            for i in range(1,n): 
+                if prices[i]>prices[i-1]:
+                    profit += prices[i]-prices[i-1]
+            return profit
+        else:
+            buy_arr = [-float('inf')]*(k+1)
+            sell_arr = [0]*(k+1)
+            for price in prices:
+                for i in range(1,k+1):
+                    buy_arr[i] = max(buy_arr[i],sell_arr[i-1]-price)
+                    sell_arr[i] = max(sell_arr[i],buy_arr[i]+price)
+            return sell_arr[k]
