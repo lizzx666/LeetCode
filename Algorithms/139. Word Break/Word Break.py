941ea6bb1ea65c92efef8d63ad23f9055a3bea89
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-19 23:29:57 +0800

    Update Word Break.py

--- Algorithms/139. Word Break/Word Break.py
+++ Algorithms/139. Word Break/Word Break.py
@@ -22,3 +22,15 @@ Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
 Output: false
 
 '''
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        dp = [False] * (len(s) +1)
+        dp[0] = True
+        for i in range(1,len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+        return dp[len(s)]
