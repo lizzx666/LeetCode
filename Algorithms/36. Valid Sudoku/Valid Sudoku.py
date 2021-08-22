9be65e795b54be4505b41a77ed49ddd2e008209b
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-23 00:57:49 +0800

    Update Valid Sudoku.py

--- Algorithms/36. Valid Sudoku/Valid Sudoku.py
+++ Algorithms/36. Valid Sudoku/Valid Sudoku.py
@@ -37,3 +37,14 @@ Output: false
 Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 
 '''
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen = []
+        for i, row in enumerate(board):
+            for j, c in enumerate(row):
+                if c!= ".":
+                    seen += [(c,j),(i,c),(i//3,j//3,c)]
+        return len(seen)==len(set(seen))
+        
