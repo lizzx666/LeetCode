e532522bb79b738db15cdb472be97ab9ff13260b
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-08-24 00:53:47 +0800

    Update Rotate Image.py

--- Algorithms/48. Rotate Image/Rotate Image.py
+++ Algorithms/48. Rotate Image/Rotate Image.py
@@ -21,3 +21,43 @@ Input: matrix = [[1,2],[3,4]]
 Output: [[3,1],[4,2]]
 
 '''
+
+
+
+'''
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+'''
+
+'''
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+ '''
+
+#if no need to return
+def rotate(matrix):
+    matrix.reverse()
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = tmp
+            
+#if need to return
+def rotate(matrix):
+    matrix = matrix[::-1]
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = tmp
+    return matrix
+
