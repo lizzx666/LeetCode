ea137fff970b97bad7f284b69a65ad49720c4cf3
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-09-08 00:35:01 +0800

    Update Ugly Number.py

--- Algorithms/263. Ugly Number/Ugly Number.py
+++ Algorithms/263. Ugly Number/Ugly Number.py
@@ -26,3 +26,11 @@ Explanation: 1 has no prime factors, therefore all of its prime factors are limi
 
 
 '''
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        for p in 2,3,5:
+        #n%p==0 and n>0
+            while n%p==0<n:
+                n /= p
+        return n == 1
