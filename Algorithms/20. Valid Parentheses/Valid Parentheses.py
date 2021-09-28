e44e4587200967a3c27df7337cf8d497589b2ab4
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-09-29 01:10:38 +0800

    Update Valid Parentheses.py

--- Algorithms/20. Valid Parentheses/Valid Parentheses.py
+++ Algorithms/20. Valid Parentheses/Valid Parentheses.py
@@ -27,3 +27,10 @@ Example 5:
 Input: s = "{[]}"
 Output: true
 '''
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while "()" in s or "{}" in s or "[]" in s:
+            s = s.replace("()","").replace("{}","").replace("[]","")
+        return s == ''
