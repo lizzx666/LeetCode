dd5b259af1ceaa4413f25958a291f9cbf74bbc98
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-09-30 01:12:48 +0800

    Update Valid Palindrome.py

--- Algorithms/125. Valid Palindrome/Valid Palindrome.py
+++ Algorithms/125. Valid Palindrome/Valid Palindrome.py
@@ -11,3 +11,12 @@ Input: s = "race a car"
 Output: false
 Explanation: "raceacar" is not a palindrome.
 '''
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c for c in s if c.isalnum()).lower()
+        if s == s[::-1]:
+            return True
+        else:
+            return False
