63f65a4797b9bd72d3261cc0cb05fd003517ece9
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-10-09 16:31:30 +0800

    Update Implement Stack using Queues.py

--- Algorithms/225. Implement Stack using Queues/Implement Stack using Queues.py
+++ Algorithms/225. Implement Stack using Queues/Implement Stack using Queues.py
@@ -31,3 +31,40 @@ myStack.pop(); // return 2
 myStack.empty(); // return False
 
 '''
+
+
+
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q1 = collections.deque()
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.q1.pop()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.q1[-1]
+        
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return not len(self.q1)
