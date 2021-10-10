a8cde8fca6ae2cbb8a3b2bdf84c81d570a383b75
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-10-10 17:13:07 +0800

    Update Path Sum.py

--- Algorithms/112. Path Sum/Path Sum.py
+++ Algorithms/112. Path Sum/Path Sum.py
@@ -21,3 +21,15 @@ Output: false
 
 
 '''
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        if not root:
+            return False
+        if root.val == targetSum and not(root.left or root.right):
+            return True
+        
+        targetSum -= root.val
+        
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
