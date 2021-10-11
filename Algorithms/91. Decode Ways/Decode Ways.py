e064bd651d581fcedfbfcf4cff0c01f560140012
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-10-11 15:21:37 +0800

    Update Decode Ways.py

--- Algorithms/91. Decode Ways/Decode Ways.py
+++ Algorithms/91. Decode Ways/Decode Ways.py
@@ -42,3 +42,19 @@ Output: 0
 Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
 
 '''
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == '0':
+            return 0
+        dp = [0 for x in range(len(s) + 1)]
+        dp[0:2] = [1,1]
+        for i in range(2,len(s)+1):
+            #one step jump
+            if 0 < int(s[i-1:i]):
+                dp[i] += dp[i-1]
+            #two step jump
+            if 10 <= int(s[i-2:i]) <=26:
+                dp[i] += dp[i-2]
+        return dp[-1]
