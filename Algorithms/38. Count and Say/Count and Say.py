c434e916ce8ac1912237608412a54ef7ed7017d0
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-10-22 00:45:35 +0800

    Update Count and Say.py

--- Algorithms/38. Count and Say/Count and Say.py
+++ Algorithms/38. Count and Say/Count and Say.py
@@ -7,7 +7,7 @@ To determine how you "say" a digit string, split it into the minimal number of g
 Then for each group, say the number of characters, then say the character. 
 To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
 
-For example, the saying and conversion for digit string "3322251":
+For example, the saying and conversion for digit string "3322251":"23321511"
 
 
 Given a positive integer n, return the nth term of the count-and-say sequence.
