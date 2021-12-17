ece72041d1fcc00e3144b52fef19b32008d0fb2e
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-18 00:29:48 +0800

    Create Square Queue.py

---
+++ LintCode/319 · Square Queue/Square Queue.py
@@ -0,0 +1,20 @@
+'''
+Description
+Given an array height with size N, which represents the height of N people.
+You should select some of them to form a square(That is, the number of rows and columns is equal), 
+and the maximum height difference of each row should not exceed 2.
+
+What is the maximum number of people you can choose to form the square?
+
+
+
+Example:
+Input: [2,5,4,5]
+Output: 4
+Explanation: You can form the square [[2,4],[5,5]].
+
+Input: [1,4,5,6,7,7,7,10,11,12]
+Output: 9
+Explanation: You can form the square [[4,5,6],[7,7,7],[10,11,12]].
+
+'''
