8a6cb05b4d0bb416123a71af22917a3cb1b5b2ee
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-18 00:13:56 +0800

    Update Pascal's Triangle.py

--- Algorithms/118. Pascal's Triangle/Pascal's Triangle.py
+++ Algorithms/118. Pascal's Triangle/Pascal's Triangle.py
@@ -12,3 +12,12 @@ Input: numRows = 1
 Output: [[1]]
 
 '''
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        dp = [[1]*(i+1) for i in range(numRows)]
+        for i in range(2,numRows):
+            for j in range(1,i):
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+        return dp
