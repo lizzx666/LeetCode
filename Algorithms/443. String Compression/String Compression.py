5b0d6b95ed4030d218bdc14d7e03f232a30ee7bc
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-18 22:13:49 +0800

    Update String Compression.py

--- Algorithms/443. String Compression/String Compression.py
+++ Algorithms/443. String Compression/String Compression.py
@@ -35,3 +35,23 @@ Output: Return 6, and the first 6 characters of the input array should be: ["a",
 Explanation: The groups are "aaa", "bb", and "aa". This compresses to "a3b2a2". Note that each group is independent even if two groups have the same character.
 
 '''
+
+
+
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        walker, runner = 0,0
+        while runner < len(chars):
+            chars[walker] = chars[runner]
+            count = 1
+            while runner+1 < len(chars) and chars[runner]==chars[runner+1]:
+                runner+=1
+                count+=1
+            if count>1:
+                for c in str(count):
+                    chars[walker+1] = c
+                    walker += 1
+            walker += 1
+            runner += 1
+        return walker
