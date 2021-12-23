0aa36533e67c9db771a39beed771306020a0559d
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-24 01:35:10 +0800

    Update Shortest Path in Binary Matrix.py

--- Algorithms/1091. Shortest Path in Binary Matrix/Shortest Path in Binary Matrix.py
+++ Algorithms/1091. Shortest Path in Binary Matrix/Shortest Path in Binary Matrix.py
@@ -22,3 +22,40 @@ Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
 Output: -1
 
 '''
+
+
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        max_row = len(grid) - 1
+        max_col = len(grid[0]) - 1
+        directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+        
+        def get_neighbors(row,col):
+            for i,j in directions:
+                new_row = row + i
+                new_col = col + j
+                if not(0<=new_row <=max_row and 0<=new_col<=max_col):
+                    continue
+                if grid[new_row][new_col]!=0:
+                    continue
+                yield (new_row,new_col)
+                
+        if grid[0][0] != 0 or grid[max_row][max_col] != 0:
+            return -1
+        
+        queue = deque()
+        queue.append((0,0))
+        grid[0][0] = 1
+        
+        while queue:
+            row,col = queue.popleft()
+            distance = grid[row][col]
+            if (row,col) == (max_row,max_col):
+                return distance
+            for neigh_row,neigh_col in get_neighbors(row,col):
+                grid[neigh_row][neigh_col] = distance + 1
+                queue.append((neigh_row,neigh_col))
+        
+        return -1
