8937bedf9477690c58d1ee56ad4ed666e48d4701
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-24 17:21:44 +0800

    Update Next Greater Element I.py

--- Algorithms/496. Next Greater Element I/Next Greater Element I.py
+++ Algorithms/496. Next Greater Element I/Next Greater Element I.py
@@ -28,6 +28,18 @@ Explanation: The next greater element for each value of nums1 is as follows:
 
 '''
 
+#Stack
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = {}
+        stack = []
+        for num in reversed(nums2):
+            while stack and num >= stack[-1]:
+                stack.pop()
+            res[num] = stack[-1] if stack else -1
+            stack.append(num)
+        return [res[num] for num in nums1]
+       
 
 #Brute Force
 class Solution:
