c403a8e57cc2a66eb6280f09a9f6baa5a01dc166
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-25 00:58:50 +0800

    Update 3Sum.py

--- Algorithms/15. 3Sum/3Sum.py
+++ Algorithms/15. 3Sum/3Sum.py
@@ -19,3 +19,28 @@ Input: nums = [0]
 Output: []
 
 '''
+
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        output = []
+        n = len(nums)
+        for first in range(n):
+            if first > 0 and nums[first] == nums[first-1]:
+                continue
+            third = n-1
+            target = -nums[first]
+            for second in range(first+1,n):
+                if second > first+1 and nums[second] == nums[second-1]:
+                    continue
+                while second < third and nums[second] + nums[third] > target:
+                    third -= 1
+                
+                if second == third:
+                    break
+                if nums[second] + nums[third]==target:
+                    output.append([nums[first], nums[second], nums[third]])
+                    
+        return output
