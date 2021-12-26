9ca50475cb56df129b59f0f54490ca16d5687144
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-27 01:34:03 +0800

    Update Merge k Sorted Lists.py

--- Algorithms/23. Merge k Sorted Lists/Merge k Sorted Lists.py
+++ Algorithms/23. Merge k Sorted Lists/Merge k Sorted Lists.py
@@ -27,3 +27,29 @@ Input: lists = [[]]
 Output: []
 
 '''
+
+
+class Solution:
+    def merge2Lists(self,l1,l2):
+        prehead = ListNode(-1)
+        prev = prehead
+        
+        while l1 and l2:
+            if l1.val<=l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        prev.next = l1 if l1 is not None else l2
+        return prehead.next
+    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount-interval, interval*2):
+                lists[i] = self.merge2Lists(lists[i], lists[i+interval])
+            interval *= 2
+        return lists[0] if amount > 0 else None
