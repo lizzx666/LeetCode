9db9012f3588d7009630c12760eba5aac28ba936
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-26 10:21:58 +0800

    Update Merge Sorted Array.py

--- Algorithms/88. Merge Sorted Array/Merge Sorted Array.py
+++ Algorithms/88. Merge Sorted Array/Merge Sorted Array.py
@@ -35,3 +35,22 @@ Note that because m = 0, there are no elements in nums1. The 0 is only there to 
 
 
 '''
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m-1
+        p2 = n-1
+        
+        for p in range(n+m-1,-1,-1):
+            if p2<0:
+                break
+            if p1>=0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1-=1
+            else:
+                nums1[p] = nums2[p2]
+                p2-=1
