a8c3872f4fa602ffa24cb12f859da4dc66634117
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-26 23:33:03 +0800

    Update Lowest Common Ancestor of a Binary Tree.py

--- Algorithms/236. Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.py
+++ Algorithms/236. Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.py
@@ -22,3 +22,32 @@ Input: root = [1,2], p = 1, q = 2
 Output: 1
 
 '''
+#merge left null and right null with other situation
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root==p or root==q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if not left:
+            return right
+        if not right:
+            return left
+        return root
+
+#DFS
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root==p or root==q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if not left and not right:
+            return
+        if not left:
+            return right
+        if not right:
+            return left
+        return root
+       
+       
