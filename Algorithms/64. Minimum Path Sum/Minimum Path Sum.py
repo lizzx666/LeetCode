87c4ec1dfb2eb3134ec06920d01a02be0049b41a
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-27 00:16:15 +0800

    Update Minimum Path Sum.py

--- Algorithms/64. Minimum Path Sum/Minimum Path Sum.py
+++ Algorithms/64. Minimum Path Sum/Minimum Path Sum.py
@@ -16,3 +16,22 @@ Input: grid = [[1,2,3],[4,5,6]]
 Output: 12
 
 '''
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                #dp[1][j] = dp[1][j-1] + grid[0][j-1]
+                #dp[i][1] = dp[i-1][1] + grid[i-1][0]
+                if i == 1: 
+                    dp[i][j] = dp[i][j-1] + grid[i-1][j-1]
+                elif j==1:
+                    dp[i][j] = dp[i-1][j] + grid[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-1]) + grid[i-1][j-1]
+
+        return dp[m][n]
