91cafd5af6a9dc0f9d686d7022bfd504151e27cc
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2021-12-26 19:51:20 +0800

    Update Maximal Square.py

--- Algorithms/221. Maximal Square/Maximal Square.py
+++ Algorithms/221. Maximal Square/Maximal Square.py
@@ -17,8 +17,30 @@ Output: 0
 
 '''
 
+#dp
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m = len(matrix)
+        n = len(matrix[0])
+        if m == 0 or n==0:
+            return 0 
+        #dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        dp = [[0]* (n+1) for _ in range(m+1)]
+
+        max_side = 0
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if matrix[i-1][j-1]=='1':
+                    if i==1 or j==1:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i-1][j-1],dp[i][j-1],dp[i-1][j])+1
+                    max_side = max(max_side,dp[i][j])
+            
+        return max_side*max_side
 
 
+#also dp
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         m = len(matrix)
