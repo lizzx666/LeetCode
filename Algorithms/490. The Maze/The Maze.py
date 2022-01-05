c04e76af3d4526c80ab1a607dbc3bc6b539952af
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-01-06 00:07:36 +0800

    Create The Maze.py

---
+++ Algorithms/490. The Maze/The Maze.py
@@ -0,0 +1,49 @@
+'''
+There is a ball in a maze with empty spaces (represented as 0) and walls (represented as 1). 
+The ball can go through the empty spaces by rolling up, down, left or right, but it won't stop rolling until hitting a wall. 
+When the ball stops, it could choose the next direction.
+
+Given the m x n maze, the ball's start position and the destination, 
+where start = [startrow, startcol] and destination = [destinationrow, destinationcol], 
+return true if the ball can stop at the destination, otherwise return false.
+
+You may assume that the borders of the maze are all walls (see examples).
+
+ 
+
+Example 1:
+Input: maze = [[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]], start = [0,4], destination = [4,4]
+Output: true
+Explanation: One possible way is : left -> down -> left -> down -> right -> down -> right.
+
+
+Example 2:
+Input: maze = [[0,0,1,0,0],[0,0,0,0,0],[0,0,0,1,0],[1,1,0,1,1],[0,0,0,0,0]], start = [0,4], destination = [3,2]
+Output: false
+Explanation: There is no way for the ball to stop at the destination. Notice that you can pass through the destination but you cannot stop there.
+
+Example 3:
+Input: maze = [[0,0,0,0,0],[1,1,0,0,1],[0,0,0,0,0],[0,1,0,0,1],[0,1,0,0,0]], start = [4,3], destination = [0,1]
+Output: false
+
+
+'''
+
+class Solution:
+    def hasPath(self, maze, start, destination):
+        m, n, stopped = len(maze), len(maze[0]), set()
+        def dfs(x, y):
+            if (x, y) in stopped: 
+                return False
+            stopped.add((x, y))
+            if [x, y] == destination:
+                return True
+            for i, j in (-1, 0) , (1, 0), (0, -1), (0, 1):
+                newX, newY = x, y
+                while 0 <= newX + i < m and 0 <= newY + j < n and maze[newX + i][newY + j] != 1:
+                    newX += i
+                    newY += j
+                if dfs(newX, newY):
+                    return True
+            return False
+        return dfs(*start)
