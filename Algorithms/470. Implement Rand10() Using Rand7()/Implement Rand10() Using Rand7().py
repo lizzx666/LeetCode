c16703c514cc71e896abd4f8e1110a311dc607a3
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-01-10 00:33:17 +0800

    Update Implement Rand10() Using Rand7().py

--- Algorithms/470. Implement Rand10() Using Rand7()/Implement Rand10() Using Rand7().py
+++ Algorithms/470. Implement Rand10() Using Rand7()/Implement Rand10() Using Rand7().py
@@ -23,3 +23,33 @@ Input: n = 3
 Output: [3,8,10]
 
 '''
+
+
+#if randN() can generate a uniform random integer in the range [1, N]
+#then (randX() - 1)*Y +randY() can generate uniform random integer in the range[1,XY]
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        a = rand7()
+        b = rand7()
+        num = (a-1)*7 + b
+        #[1,49]
+        if num <=40:
+            return num%10+1
+        
+        #To increase the probability
+        a = num-40 #rand9()
+        b = rand7()
+        num = (a-1)*7+b
+        #[1,63]
+        if num <=60:
+            return num%10+1
+        
+        a = num-60 #rand3()
+        b = rand7()
+        num = (a-1)*7+b
+        if num<=30:
+            return num%10+1
