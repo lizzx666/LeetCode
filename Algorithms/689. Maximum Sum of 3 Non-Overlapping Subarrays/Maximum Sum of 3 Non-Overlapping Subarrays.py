11f7f103b41e5c38f7da3c4954d2845b912458d1
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-01-09 23:30:19 +0800

    Update Maximum Sum of 3 Non-Overlapping Subarrays.py

--- Algorithms/689. Maximum Sum of 3 Non-Overlapping Subarrays/Maximum Sum of 3 Non-Overlapping Subarrays.py
+++ Algorithms/689. Maximum Sum of 3 Non-Overlapping Subarrays/Maximum Sum of 3 Non-Overlapping Subarrays.py
@@ -18,3 +18,34 @@ Input: nums = [1,2,1,2,1,2,1,2,1], k = 2
 Output: [0,2,4]
 
 '''
+
+
+
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        #array_1 : [0,k-1]
+        #array_2 : [k,2k-1]
+        #array_3 : [2k,3k-1]
+        ans = []
+        sum1, maxSum1, maxSum1Idx = 0,0,0
+        sum2, maxSum12, maxSum12Idx = 0,0,()
+        sum3, maxTotal = 0,0
+        n = len(nums)
+        for i in range(2*k,n):
+            sum1 += nums[i-2*k]
+            sum2 += nums[i-k]
+            sum3 += nums[i]
+            if i >= 3*k-1:
+                if sum1 > maxSum1:
+                    maxSum1 = sum1
+                    maxSum1Idx = i-3*k+1
+                if maxSum1 + sum2 > maxSum12:
+                    maxSum12 = maxSum1 + sum2
+                    maxSum12Idx = (maxSum1Idx,i-2*k+1)
+                if maxSum12 + sum3 > maxTotal:
+                    maxTotal = maxSum12 + sum3
+                    ans = [*maxSum12Idx,i-k+1]
+                sum1 -= nums[i-3*k+1]
+                sum2 -= nums[i-2*k+1]
+                sum3 -= nums[i-k+1]
+        return ans
