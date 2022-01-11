350662422b00f10a71149081ffd35d28226f8ff3
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-01-12 01:49:59 +0800

    Update Smallest Range Covering Elements from K Lists.py

--- Algorithms/632. Smallest Range Covering Elements from K Lists/Smallest Range Covering Elements from K Lists.py
+++ Algorithms/632. Smallest Range Covering Elements from K Lists/Smallest Range Covering Elements from K Lists.py
@@ -19,3 +19,25 @@ Input: nums = [[1,2,3],[1,2,3],[1,2,3]]
 Output: [1,1]
 
 '''
+'''
+The question is equal to pick k number from k sorted list, and need to ensure the difference between max and min of these k numbers are smallest
+'''
+
+
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        range_left, range_right = -1e9,1e9
+        max_value = max(vec[0] for vec in nums)
+        pq = [(vec[0],i,0) for i,vec in enumerate(nums)]
+        heapq.heapify(pq)
+        
+        while True:
+            min_value, row, idx = heapq.heappop(pq)
+            if max_value - min_value < range_right - range_left:
+                range_left, range_right = min_value, max_value
+            if idx == len(nums[row])-1:
+                break
+            max_value = max(max_value, nums[row][idx+1])
+            heapq.heappush(pq, (nums[row][idx+1],row,idx+1))
+        
+        return [range_left,range_right]
