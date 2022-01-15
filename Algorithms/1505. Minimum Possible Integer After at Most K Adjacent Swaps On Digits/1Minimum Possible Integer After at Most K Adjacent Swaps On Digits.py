54c8721dd78fbc1ab511c3d6dbb127cfe63289dc
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-01-15 22:34:11 +0800

    Update 1Minimum Possible Integer After at Most K Adjacent Swaps On Digits.py

--- Algorithms/1505. Minimum Possible Integer After at Most K Adjacent Swaps On Digits/1Minimum Possible Integer After at Most K Adjacent Swaps On Digits.py
+++ Algorithms/1505. Minimum Possible Integer After at Most K Adjacent Swaps On Digits/1Minimum Possible Integer After at Most K Adjacent Swaps On Digits.py
@@ -23,3 +23,19 @@ Output: "36789"
 Explanation: We can keep the number without any swaps.
 
 '''
+
+#Brute Force
+#O(n^2), O(n)
+
+class Solution:
+    def minInteger(self, num: str, k: int) -> str:
+        if k<=0:
+            return num
+        n = len(num)
+        if k>= n*(n-1)//2:
+            return "".join(sorted(list(num)))
+        
+        for i in range(10):
+            ind = num.find(str(i))
+            if 0<=ind<=k:
+                return str(num[ind]) + self.minInteger(num[0:ind] + num[ind+1:], k-ind)
