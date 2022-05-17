2e78b1b7d49ad48d3d3dead8dd8f6bc588e52d2b
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-05-18 00:24:09 +0800

    Update Pascal's Triangle II.py

--- Algorithms/119. Pascal's Triangle II/Pascal's Triangle II.py
+++ Algorithms/119. Pascal's Triangle II/Pascal's Triangle II.py
@@ -24,3 +24,17 @@ class Solution:
         for _ in range(0,rowIndex):
             row = [x+y for x,y in zip([0]+row , row+[0])]
         return row
+    
+    
+    
+    
+    
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        dp = [[1]* (i+1) for i in range(rowIndex+1)]
+
+        for i in range(2,rowIndex+1):
+            for j in range(1,i):
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                
+        return dp[rowIndex]
