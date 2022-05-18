044f73a3c29aa23c060675036bb461f47e0efc33
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-05-19 00:13:09 +0800

    Update Find Peak Element.py

--- Algorithms/162. Find Peak Element/Find Peak Element.py
+++ Algorithms/162. Find Peak Element/Find Peak Element.py
@@ -22,3 +22,26 @@ Output: 5
 Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
 
 '''
+
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        left = 0
+        right = n-1
+        
+        while left < right:
+            mid = left + (right-left)//2
+            
+            if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
+                return mid
+            
+            if nums[mid] < nums[mid+1]:
+                left = mid+1
+            
+            else:
+                right = mid-1
+        
+        return left
