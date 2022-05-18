9cb6f03721102ab6cf2dc63aeedfb9b2461a9f1a
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-05-19 00:29:07 +0800

    Update Remove Duplicates from Sorted List II.py

--- Algorithms/82. Remove Duplicates from Sorted List II/Remove Duplicates from Sorted List II.py
+++ Algorithms/82. Remove Duplicates from Sorted List II/Remove Duplicates from Sorted List II.py
@@ -14,3 +14,48 @@ Input: head = [1,1,1,2,3]
 Output: [2,3]
 
 '''
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = pre = ListNode(0)
+        dummy.next = head
+        
+        
+        while head and head.next:
+            if head.next.val == head.val:
+                while head and head.next and head.next.val == head.val:
+                    head = head.next
+                head = head.next
+                pre.next = head
+            
+            else:
+                pre = pre.next
+                head = head.next
+                
+        return dummy.next
+       
+       
+ 
+ 
+ class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = pre = ListNode(0)
+        dummy.next = head
+        
+        cur = head
+        
+        while cur:
+            if cur.next and cur.val == cur.next.val:
+                while cur and cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                pre.next = cur.next
+                
+            else:
+                pre = pre.next
+            
+            cur = cur.next
+            
+        return dummy.next
+       
+       
