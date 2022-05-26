0b4261c83bf11d4b79715a7ba3de9868fbec4957
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-05-27 01:03:06 +0800

    Update Container With Most Water.py

--- Algorithms/11. Container With Most Water/Container With Most Water.py
+++ Algorithms/11. Container With Most Water/Container With Most Water.py
@@ -23,3 +23,18 @@ Input: height = [1,1]
 Output: 1
 
 '''
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i, j = 0, len(height)-1
+        water = 0
+        while i<j:
+            water = max(water,min(height[i],height[j])*(j-i))
+            if height[i]<height[j]:
+                i+=1
+            else:
+                j-=1
+        
+        
+        return water
