6a092138e8b111538fcaa7a6b93ccc19e8232110
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-05-27 00:31:40 +0800

    Update Interval List Intersections.py

--- Algorithms/986. Interval List Intersections/Interval List Intersections.py
+++ Algorithms/986. Interval List Intersections/Interval List Intersections.py
@@ -57,3 +57,24 @@ class Solution:
 
     
         return result 
+    
+    
+ class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        m = len(firstList)
+        n = len(secondList)
+        i=j=0
+        result = []
+        
+        while i<m and j<n:
+            a_start,a_end = firstList[i]
+            b_start,b_end = secondList[j]
+            
+            if a_start<=b_end and b_start<=a_end:
+                result.append([max(a_start,b_start),min(a_end,b_end)])
+                
+            if a_end<=b_end:
+                i+=1
+            else:
+                j+=1
+        return result
