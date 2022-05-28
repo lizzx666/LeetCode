830a44999008a856520aa2910ac71550ce775d28
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-05-28 21:59:13 +0800

    Update Product of Array Except Self.py

--- Algorithms/238. Product of Array Except Self/Product of Array Except Self.py
+++ Algorithms/238. Product of Array Except Self/Product of Array Except Self.py
@@ -17,3 +17,20 @@ Output: [0,0,9,0,0]
 
 
 '''
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        p = 1
+        output = []
+        for i in range(n):
+            output.append(p)
+            p = p*nums[i]
+        p = 1
+        
+        for i in range(n-1,-1,-1):
+            output[i] = output[i]*p
+            p = p*nums[i]
+        return output
+
