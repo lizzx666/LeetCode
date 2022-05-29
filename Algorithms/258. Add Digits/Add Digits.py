8be1e9c7980e53523334bd4f2be24eae30a1c337
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-05-29 17:20:37 +0800

    Update Add Digits.py

--- Algorithms/258. Add Digits/Add Digits.py
+++ Algorithms/258. Add Digits/Add Digits.py
@@ -30,3 +30,12 @@ class Solution:
                 num = num//10
             num = result
         return num
+
+       
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        if num % 9==0:
+            return 9
+        return num % 9
