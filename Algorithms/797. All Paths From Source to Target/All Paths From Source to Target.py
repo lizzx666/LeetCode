44ba52c760b502fa3073a1d5887ac3393e07b65d
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-06-07 22:35:25 +0800

    Update All Paths From Source to Target.py

--- Algorithms/797. All Paths From Source to Target/All Paths From Source to Target.py
+++ Algorithms/797. All Paths From Source to Target/All Paths From Source to Target.py
@@ -18,3 +18,24 @@ Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
 
 
 '''
+
+
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        ans = list()
+        stk = list()
+        
+        def dfs(x):
+            if x==len(graph)-1:
+                ans.append(stk[:])
+                return
+            
+            for y in graph[x]:
+                stk.append(y)
+                dfs(y)
+                stk.pop()
+
+        stk.append(0)
+        dfs(0)
+        return ans
