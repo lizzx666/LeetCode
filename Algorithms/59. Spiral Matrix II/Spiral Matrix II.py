9b39b278f050eb14c2d5e42a0d52ae50ec56cad0
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-07-30 21:58:53 +0800

    Update Spiral Matrix II.py

--- Algorithms/59. Spiral Matrix II/Spiral Matrix II.py
+++ Algorithms/59. Spiral Matrix II/Spiral Matrix II.py
@@ -15,6 +15,40 @@ Output: [[1]]
 
 
 
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        nums = [[0]*n for _ in range(n)]
+        startx, starty = 0,0
+        cnt = 1
+        mid = n//2
+        for offset in range(1,n//2+1):
+            #from left to righ
+            for j in range(starty,n-offset):
+                nums[startx][j]=cnt
+                cnt+=1
+            #from top to bottom
+            for i in range(startx,n-offset):
+                nums[i][n-offset]=cnt
+                cnt+=1
+            #from right to left
+            for j in range(n-offset,starty,-1):
+                nums[n-offset][j]=cnt
+                cnt+=1
+            #from bottom to top
+            for i in range(n-offset,startx,-1):
+                nums[i][starty]=cnt
+                cnt+=1
+            
+            startx+=1
+            starty+=1
+        
+        if n%2==1:
+            nums[mid][mid]=cnt
+        return nums
+
+
+
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         A = [[0]*n for _ in range(n)]  
