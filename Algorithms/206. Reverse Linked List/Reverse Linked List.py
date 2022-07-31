718af987013793288db6072451902c7636a3de06
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-07-31 21:13:54 +0800

    Update Reverse Linked List.py

--- Algorithms/206. Reverse Linked List/Reverse Linked List.py
+++ Algorithms/206. Reverse Linked List/Reverse Linked List.py
@@ -15,6 +15,26 @@ Input: head = []
 Output: []
 
 '''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pre = None
+        curr = head
+        
+        while curr:
+            temp = curr.next
+            curr.next = pre
+            pre = curr
+            curr = temp
+            
+        return pre
+
+
+
 
 # Definition for singly-linked list.
 # class ListNode:
