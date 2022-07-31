287828d865c148c70cd08161bf0146b1bde064cb
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-07-31 21:59:46 +0800

    Update Swap Nodes in Pairs.py

--- Algorithms/24. Swap Nodes in Pairs/Swap Nodes in Pairs.py
+++ Algorithms/24. Swap Nodes in Pairs/Swap Nodes in Pairs.py
@@ -26,6 +26,30 @@ Output: [1]
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_head = ListNode(-1)
+        dummy_head.next = head
+        curr = dummy_head
+        
+        while curr.next and curr.next.next:
+            temp1 = curr.next
+            temp2 = curr.next.next.next
+            
+            curr.next = curr.next.next
+            curr.next.next = temp1
+            temp1.next = temp2
+            
+            curr = curr.next.next
+            
+        return dummy_head.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(-1)
         dummy.next = head
         prev_node = dummy
