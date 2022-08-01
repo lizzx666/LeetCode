75e62c686ed6fe2a6b71eae67c615bc67c19b67c
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-08-01 23:17:43 +0800

    Update 3Sum.py

--- Algorithms/15. 3Sum/3Sum.py
+++ Algorithms/15. 3Sum/3Sum.py
@@ -23,6 +23,39 @@ Output: []
 
 
 
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        n = len(nums)
+        for i in range(n):
+
+            if nums[i]>0:
+                break
+            if i>0 and nums[i-1]==nums[i]:
+                continue
+            left = i+1
+            right = n-1
+
+            while left<right:
+                if nums[i]+nums[left]+nums[right]<0:
+                    left+=1
+                elif nums[i]+nums[left]+nums[right]>0:
+                    right-=1
+                else:
+                    result.append([nums[i],nums[left],nums[right]])
+                    while left<right and nums[left]==nums[left+1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right-1]:
+                        right-=1
+                    
+                    left+=1
+                    right-=1
+        return result
+
+
+
+
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
