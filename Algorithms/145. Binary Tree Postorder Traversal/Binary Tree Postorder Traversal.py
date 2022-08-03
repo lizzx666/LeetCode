55901696428b0fe83a6719de1bb1165bcbd9de5d
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-08-03 23:54:23 +0800

    Update Binary Tree Postorder Traversal.py

--- Algorithms/145. Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.py
+++ Algorithms/145. Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.py
@@ -19,3 +19,25 @@ Output: [1]
 
 
 '''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        
+        def traversal(root):
+            if not root:
+                return
+            traversal(root.left)
+            traversal(root.right)
+            result.append(root.val)
+            
+        traversal(root)
+        return result
