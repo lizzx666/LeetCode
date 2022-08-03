924be8bc66ae8bb07e6de95fa49aa5068a7d248a
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-08-04 00:16:56 +0800

    Update Binary Tree Postorder Traversal.py

--- Algorithms/145. Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.py
+++ Algorithms/145. Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.py
@@ -41,3 +41,22 @@ class Solution:
             
         traversal(root)
         return result
+       
+       
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        result = []
+        
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+                
+        return result[::-1]
