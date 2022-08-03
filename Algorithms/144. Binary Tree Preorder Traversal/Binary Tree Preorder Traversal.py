842bf55a965c0bba744344c1e3f74c22ae70a4ac
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-08-04 00:09:23 +0800

    Update Binary Tree Preorder Traversal.py

--- Algorithms/144. Binary Tree Preorder Traversal/Binary Tree Preorder Traversal.py
+++ Algorithms/144. Binary Tree Preorder Traversal/Binary Tree Preorder Traversal.py
@@ -45,23 +45,17 @@ class Solution:
         traversal(root)
         return result
 
-
-
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-
         if not root:
             return []
-        stack, output = [root,], []
-        
+        stack = [root]
+        result = []
         while stack:
-            root = stack.pop()
-            if root:
-                output.append(root.val)
-                if root.right is not None:
-                    stack.append(root.right)
-                if root.left is not None:
-                    stack.append(root.left)
-  
-        
-        return output
+            node = stack.pop()
+            result.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return result
