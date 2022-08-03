3d5c69f928dc4b65285ff07fe698a4c7a7a2b07a
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-08-04 00:36:54 +0800

    Update Binary Tree Inorder Traversal.py

--- Algorithms/94. Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.py
+++ Algorithms/94. Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.py
@@ -27,6 +27,28 @@ Output: [1]
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        if not root:
+            return []
+        stack = []
+        result = []
+        cur = root
+        
+        while cur or stack:
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                result.append(cur.val)
+                cur = cur.right
+                
+        return result
+       
+       
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
