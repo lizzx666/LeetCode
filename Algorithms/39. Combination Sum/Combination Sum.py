3890e8fc9200590d65428f9b415b43608bfbcc2b
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-08-06 22:13:14 +0800

    Create Combination Sum.py

---
+++ Algorithms/39. Combination Sum/Combination Sum.py
@@ -0,0 +1,64 @@
+'''
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. 
+You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. 
+Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+
+Example 2:
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+Example 3:
+Input: candidates = [2], target = 1
+Output: []
+
+
+
+'''
+
+
+
+
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        path = []
+        result = []
+        candidates.sort()
+        
+        def backtracking(candidates,target,curr_sum,startindex):
+            if curr_sum > target:
+                return
+            if curr_sum == target:
+                result.append(path[:])
+                return
+
+            for i in range(startindex,len(candidates)):
+                if curr_sum+candidates[i]>target:
+                    return
+                curr_sum+=candidates[i]
+                path.append(candidates[i])
+                backtracking(candidates,target,curr_sum,i)
+                curr_sum-=candidates[i]
+                path.pop()
+                
+        backtracking(candidates,target,0,0)
+        return result
+        
+
+
