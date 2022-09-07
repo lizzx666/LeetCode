2a651ef2bea7c790c5e50f9b618bc1b836a19077
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-09-07 12:07:10 +0800

    Update Symmetric Tree.py

--- Algorithms/101. Symmetric Tree/Symmetric Tree.py
+++ Algorithms/101. Symmetric Tree/Symmetric Tree.py
@@ -15,3 +15,29 @@ Output: false
 
 
 '''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        return self.compare(root.left,root.right)
+    
+    def compare(self,left,right):
+        if left == None and right!=None:
+            return False
+        elif left!=None and right==None:
+            return False
+        elif left==None and right==None:
+            return True
+        elif left.val!=right.val:
+            return False
+        
+        return self.compare(left.left,right.right) and self.compare(left.right,right.left)
