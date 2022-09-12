27fbe679e52993ffc19a15d54f5e176fd6c62d65
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-09-12 23:17:45 +0800

    Update Sum of Left Leaves.py

--- Algorithms/404. Sum of Left Leaves/Sum of Left Leaves.py
+++ Algorithms/404. Sum of Left Leaves/Sum of Left Leaves.py
@@ -17,3 +17,30 @@ Output: 0
 
 
 '''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return self.traversal(root)
+    
+    def traversal(self,node):
+        if node==None:
+            return 0
+        if node.left==None and node.right==None:
+            return 0
+        left_val = self.traversal(node.left)
+        if node.left and node.left.left==None and node.left.right==None:
+            left_val = node.left.val
+            
+        right_val = self.traversal(node.right)
+        sum_val = left_val + right_val
+        return sum_val
