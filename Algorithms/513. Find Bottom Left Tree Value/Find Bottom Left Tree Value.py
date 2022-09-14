61ef7b30e80a3db8b409ccbd02ddc588c73d431d
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-09-15 00:12:18 +0800

    Update Find Bottom Left Tree Value.py

--- Algorithms/513. Find Bottom Left Tree Value/Find Bottom Left Tree Value.py
+++ Algorithms/513. Find Bottom Left Tree Value/Find Bottom Left Tree Value.py
@@ -14,3 +14,33 @@ Input: root = [1,2,3,4,null,5,6,null,null,7]
 Output: 7
 
 '''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        max_depth = float('-inf')
+        leftmost_val = 0
+        
+        def traversal(node,depth):
+            nonlocal max_depth, leftmost_val
+            if node.left==None and node.right==None:
+                if depth>max_depth:
+                    max_depth=depth
+                    leftmost_val = node.val
+            if node.left:
+                depth+=1
+                traversal(node.left,depth)
+                depth-=1
+            if node.right:
+                depth+=1
+                traversal(node.right,depth)
+                depth-=1
+                
+        traversal(root,0) 
+        return leftmost_val
