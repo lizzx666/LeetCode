c4409dc7927dcb8add8c225595920a4d9c8efd7e
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-09-17 23:49:49 +0800

    Update Construct Binary Tree from Inorder and Postorder Traversal.py

--- Algorithms/106. Construct Binary Tree from Inorder and Postorder Traversal/Construct Binary Tree from Inorder and Postorder Traversal.py
+++ Algorithms/106. Construct Binary Tree from Inorder and Postorder Traversal/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -16,3 +16,35 @@ Output: [-1]
 
 
 '''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if not postorder:
+            return None
+        #last item of postorder is current root
+        root_val = postorder[-1]
+        root = TreeNode(root_val)
+        
+        #find cutting point
+        separator_idx = inorder.index(root_val)
+        
+        #cut inorder array
+        inorder_left = inorder[:separator_idx]
+        inorder_right = inorder[separator_idx+1:]
+        
+        #cut postorder array, after-cut postorder array length should be same with after-cut inorder array
+        postorder_left = postorder[:len(inorder_left)]
+        postorder_right = postorder[len(inorder_left):len(postorder)-1]
+        
+        #recursion
+        root.left = self.buildTree(inorder_left,postorder_left)
+        root.right = self.buildTree(inorder_right,postorder_right)
+        
+        return root
