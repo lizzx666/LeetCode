38c0b43e9a600ea2065c00d0a729673f2d49ba22
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2022-09-18 00:33:20 +0800

    Update Path Sum II.py

--- Algorithms/113. Path Sum II/Path Sum II.py
+++ Algorithms/113. Path Sum II/Path Sum II.py
@@ -25,3 +25,39 @@ Output: []
 
 
 '''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        result = []
+        path = []
+        if not root:
+            return []
+        path.append(root.val)
+        
+        def traversal(node,target):
+            if node.left==None and node.right==None and target==0:
+                result.append(path[:])
+            
+            if node.left==None and node.right==None and target!=0:
+                return
+        
+            if node.left:
+                path.append(node.left.val)
+                traversal(node.left,target-node.left.val)
+                path.pop()
+            
+            if node.right:
+                path.append(node.right.val)
+                traversal(node.right, target-node.right.val)
+                path.pop()
+        
+        traversal(root,targetSum-root.val)
+        return result
