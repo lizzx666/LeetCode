05610d75b2b80f051978ef96abd55c5a8f9fade3
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-03-13 21:55:37 +0800

    Update Jump Game II.py

--- Algorithms/45. Jump Game II/Jump Game II.py
+++ Algorithms/45. Jump Game II/Jump Game II.py
@@ -10,16 +10,36 @@ You can assume that you can always reach the last index.
  
 
 Example 1:
-
 Input: nums = [2,3,1,1,4]
 Output: 2
 Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
-Example 2:
 
+Example 2:
 Input: nums = [2,3,0,1,4]
 Output: 2
 
 '''
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return 0
+        cur_cover = 0
+        next_cover = 0   
+        result = 0
+        for i in range(len(nums)):
+            next_cover = max(next_cover,i+nums[i])
+            if i==cur_cover:
+                if cur_cover < len(nums)-1:
+                    result+=1 
+                    cur_cover = next_cover  
+                    if cur_cover >= len(nums)-1:
+                        break
+        return result
+       
+       
+       
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
