b12de81a80b148fb127add646a75c5034981d1a2
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-03-13 20:57:32 +0800

    Update Best Time to Buy and Sell Stock II.py

--- Algorithms/122. Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.py
+++ Algorithms/122. Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.py
@@ -27,6 +27,14 @@ Explanation: In this case, no transaction is done, i.e., max profit = 0.
 
 '''
 
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        for i in range(1,len(prices)):
+            result += max(0,prices[i]-prices[i-1])
+        return result
+
 def maxProfit(prices):
     max_profit = 0
     for i in range(1,len(prices)):
