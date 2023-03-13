c0b5b32219c2ab81917ace4f1857ae40b25b6fd4
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-03-13 23:29:36 +0800

    Update Gas Station.py

--- Algorithms/134. Gas Station/Gas Station.py
+++ Algorithms/134. Gas Station/Gas Station.py
@@ -36,3 +36,20 @@ Therefore, you can't travel around the circuit once no matter where you start.
 
 
 '''
+
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        cur_sum = 0
+        total_sum = 0
+        start = 0
+        for i in range(len(gas)):
+            cur_sum += gas[i]-cost[i]
+            total_sum += gas[i]-cost[i]
+            if cur_sum<0:
+                start = i+1
+                cur_sum = 0
+        if total_sum < 0:
+            return -1
+        return start
