6cbe46927066d60fbb281711080c52a4767e613a
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-03-14 22:57:18 +0800

    Update Lemonade Change.py

--- Algorithms/860. Lemonade Change/Lemonade Change.py
+++ Algorithms/860. Lemonade Change/Lemonade Change.py
@@ -31,3 +31,30 @@ Since not every customer received the correct change, the answer is false.
 
 
 '''
+
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten = 0
+        twenty = 0
+        for i in range(len(bills)):
+            if bills[i]==5:
+                five+=1
+            elif bills[i]==10:
+                if five:
+                    ten+=1
+                    five-=1
+                else:
+                    return False
+            else:
+                if ten and five:
+                    ten-=1
+                    five-=1
+                    twenty+=1
+                elif five>=3:
+                    five-=3
+                    twenty+=1
+                else:
+                    return False
+        return True
