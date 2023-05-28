f6233965c61d9c35217f4859728a7756cbb87c86
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-05-28 23:47:57 +0800

    Update Minimum Number of Arrows to Burst Balloons.py

--- Algorithms/452. Minimum Number of Arrows to Burst Balloons/Minimum Number of Arrows to Burst Balloons.py
+++ Algorithms/452. Minimum Number of Arrows to Burst Balloons/Minimum Number of Arrows to Burst Balloons.py
@@ -35,3 +35,20 @@ Explanation: The balloons can be burst by 2 arrows:
 
 
 '''
+
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if len(points) == 0:
+            return 0
+        
+        points.sort(key = lambda x:x[0])
+        result = 1
+
+        for i in range(1,len(points)):
+            if points[i-1][1]<points[i][0]:
+                result += 1
+            else:
+                points[i][1] = min(points[i-1][1],points[i][1])
+        return result
