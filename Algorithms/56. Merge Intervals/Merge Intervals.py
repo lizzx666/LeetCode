44ef6b951ce0d0b4ccdf3d88f01d08de6a014c11
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-05-31 00:01:48 +0800

    Update Merge Intervals.py

--- Algorithms/56. Merge Intervals/Merge Intervals.py
+++ Algorithms/56. Merge Intervals/Merge Intervals.py
@@ -15,6 +15,22 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 
 '''
 
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+        if len(intervals)==0:
+            return result
+        intervals.sort(key = lambda x:x[0])
+        result.append(intervals[0])
+        for i in range(1,len(intervals)):
+            if intervals[i][0]<=result[-1][1]:
+                result[-1][1] = max(intervals[i][1],result[-1][1])
+            else:
+                result.append(intervals[i])
+        return result
+    
+    
+    
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
