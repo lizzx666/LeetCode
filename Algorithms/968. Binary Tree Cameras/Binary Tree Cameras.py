b6f4e81cae6f4f629e4a0736a824ea8cdbc12005
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-06-04 21:03:18 +0800

    Update Binary Tree Cameras.py

--- Algorithms/968. Binary Tree Cameras/Binary Tree Cameras.py
+++ Algorithms/968. Binary Tree Cameras/Binary Tree Cameras.py
@@ -18,3 +18,36 @@ Explanation: At least two cameras are needed to monitor all nodes of the tree. T
 
 
 '''
+
+
+
+
+
+class Solution:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        #status:
+        #0: not covered
+        #1: equip camera
+        #2: been covered
+
+        result = 0
+        def traversal(curr):
+            nonlocal result
+
+            if not curr:
+                return 2
+            left = traversal(curr.left)
+            right = traversal(curr.right)
+
+            if left == 2 and right == 2:
+                return 0
+            elif left == 0 or right == 0:
+                result+=1
+                return 1
+            elif left == 1 or right == 1:
+                return 2
+
+        if traversal(root)==0:
+            result += 1
+
+        return result
