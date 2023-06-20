79ccd6e91537fea1e1bd0174eaf9800872eff3ec
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-06-21 00:26:08 +0800

    Update Integer Break.py

--- Algorithms/343. Integer Break/Integer Break.py
+++ Algorithms/343. Integer Break/Integer Break.py
@@ -19,3 +19,14 @@ Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
 
 
 '''
+
+
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [0]*(n+1)
+        dp[2] = 1
+        for i in range(3,n+1):
+            for j in range(1,i//2+1):
+                dp[i] = max(j*(i-j),j*dp[i-j],dp[i])
+        return dp[-1]
