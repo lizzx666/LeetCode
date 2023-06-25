ff7ac77d2fed469318df84ca22e3f81be02b148b
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-06-26 00:08:49 +0800

    Update Unique Binary Search Trees.py

--- Algorithms/96. Unique Binary Search Trees/Unique Binary Search Trees.py
+++ Algorithms/96. Unique Binary Search Trees/Unique Binary Search Trees.py
@@ -13,10 +13,11 @@ Output: 1
 
 class Solution:
     def numTrees(self, n: int) -> int:
-        G = [0]*(n+1)
-        G[0] = 1
-        G[1] = 1
-        for i in range(2,n+1):
+        dp = [0]*(n+1)
+        dp[0] = 1
+
+        for i in range(1,n+1):
             for j in range(1,i+1):
-                G[i] += G[j-1] * G[i-j]
-        return G[n]
+                dp[i] += dp[j-1]*dp[i-j]
+
+        return dp[n]
