a5c1c0935487d6cc39a9f891e605f30e3db176b7
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-05 01:02:03 +0800

    Update Partition Equal Subset Sum.py

--- Algorithms/416. Partition Equal Subset Sum/Partition Equal Subset Sum.py
+++ Algorithms/416. Partition Equal Subset Sum/Partition Equal Subset Sum.py
@@ -16,3 +16,25 @@ Explanation: The array cannot be partitioned into equal sum subsets.
 
 
 '''
+
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        sum_nums = sum(nums)
+        if sum_nums%2:
+            return False
+
+        target = sum_nums//2
+
+        dp = [0]*(target+1)
+
+        for i in range(len(nums)):
+            for j in range(target,nums[i]-1,-1):
+                dp[j] = max(dp[j],dp[j-nums[i]]+nums[i])
+                #print(dp)
+        #print(dp)
+        if dp[target]==target:
+            return True
+        else:
+            return False
