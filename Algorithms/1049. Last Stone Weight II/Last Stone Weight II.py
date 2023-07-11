8ea73c80fca9c898d54443e166477a8f73327ebb
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-12 00:09:21 +0800

    Update Last Stone Weight II.py

--- Algorithms/1049. Last Stone Weight II/Last Stone Weight II.py
+++ Algorithms/1049. Last Stone Weight II/Last Stone Weight II.py
@@ -28,3 +28,19 @@ Output: 5
 
 
 '''
+
+
+
+
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        sum_stones = sum(stones)
+        target = sum_stones//2
+
+        dp = [0]*(target+1)
+
+        for i in range(0,len(stones)):
+            for j in range(target,stones[i]-1,-1):
+                dp[j] = max(dp[j],dp[j-stones[i]]+stones[i])
+
+        return sum_stones-dp[target]-dp[target]
