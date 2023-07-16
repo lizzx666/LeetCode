09e5cbfa83eeaa10b1ed798b5447760419d2430a
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-16 23:59:27 +0800

    Update Coin Change.py

--- Algorithms/322. Coin Change/Coin Change.py
+++ Algorithms/322. Coin Change/Coin Change.py
@@ -30,6 +30,20 @@ Output: 2
 
 '''
 
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')]*(amount+1)
+        dp[0]=0
+
+        for i in range(len(coins)):
+            for j in range(coins[i],amount+1):
+                if dp[j-coins[i]]!=float('inf'):
+                    dp[j] = min(dp[j],dp[j-coins[i]]+1)
+        if dp[-1]==float('inf'):
+            return -1
+        return dp[-1]
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [float('inf')]*(amount+1)
