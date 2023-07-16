63152343febbdf7c5a2fe486d662b9799e51175f
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-16 23:41:18 +0800

    Update Combination Sum IV.py

--- Algorithms/377. Combination Sum IV/Combination Sum IV.py
+++ Algorithms/377. Combination Sum IV/Combination Sum IV.py
@@ -26,3 +26,17 @@ Input: nums = [9], target = 3
 Output: 0
 
 '''
+
+
+
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0]*(target+1)
+        dp[0]=1
+
+        for j in range(target+1):
+            for i in range(len(nums)):
+                if j - nums[i]>=0:
+                    dp[j]+=dp[j-nums[i]]
+        return dp[-1]
