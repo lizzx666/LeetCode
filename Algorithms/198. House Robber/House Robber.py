4fa804e0bb0b229541084fba2089de886b7fe5e7
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-18 22:53:04 +0800

    Update House Robber.py

--- Algorithms/198. House Robber/House Robber.py
+++ Algorithms/198. House Robber/House Robber.py
@@ -21,13 +21,22 @@ Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (m
 Total amount you can rob = 2 + 9 + 1 = 12.
 
 '''
-
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        dp = [0]*(len(nums))
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            dp[i] = max(nums[i]+dp[i-2],dp[i-1])
+        
+        return dp[-1]
+     
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n==0:
-            return 0
         if n==1:
             return nums[0]
         dp = [0]*n
