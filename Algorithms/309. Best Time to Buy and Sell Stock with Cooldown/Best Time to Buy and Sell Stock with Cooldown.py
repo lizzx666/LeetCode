a6fcccf1e5e487b4bb8a306542243d94d05fa817
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-20 00:12:51 +0800

    Update Best Time to Buy and Sell Stock with Cooldown.py

--- Algorithms/309. Best Time to Buy and Sell Stock with Cooldown/Best Time to Buy and Sell Stock with Cooldown.py
+++ Algorithms/309. Best Time to Buy and Sell Stock with Cooldown/Best Time to Buy and Sell Stock with Cooldown.py
@@ -21,3 +21,27 @@ Output: 0
 
 
 '''
+
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [[0 for i in range(4)] for j in range(len(prices))]
+        #dp[i][0]:on day i, hold stock
+        #dp[i][1]:on day i, no stock and non-freeze period
+        #dp[i][2]:on day i, sell stock
+        #dp[i][3]:day i is freeze period
+
+        dp[0][0]=-prices[0]
+        dp[0][1]=0
+        dp[0][2]=0
+        dp[0][3]=0
+
+        for i in range(1,len(prices)):
+            dp[i][0] = max(dp[i-1][0],dp[i-1][1]-prices[i],dp[i-1][3]-prices[i])
+            dp[i][1] = max(dp[i-1][1],dp[i-1][3])
+            dp[i][2] = dp[i-1][0]+prices[i]
+            dp[i][3] = dp[i-1][2]
+        
+        max_val = max(dp[-1][1],dp[-1][2],dp[-1][3])
+        return max_val
