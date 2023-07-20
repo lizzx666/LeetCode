b17cbce7dd20752059cacb354811f9d31188858c
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-21 01:02:07 +0800

    Update Delete Operation for Two Strings.py

--- Algorithms/583. Delete Operation for Two Strings/Delete Operation for Two Strings.py
+++ Algorithms/583. Delete Operation for Two Strings/Delete Operation for Two Strings.py
@@ -19,6 +19,24 @@ Output: 4
 
 '''
 
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp=[[0 for j in range(len(word2)+1)] for i in range(len(word1)+1)]
+        #find maximum subsequence then use len(word1)+len(word2)-2*sub
+
+        for i in range(1,len(word1)+1):
+            for j in range(1,len(word2)+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        
+        sub = dp[-1][-1]
+        res = len(word1)+len(word2)-2*sub
+        return res
+
+
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
