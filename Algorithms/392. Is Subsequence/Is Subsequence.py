3359379e0c9672d031b8d2669b51432e2ccc3987
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-20 23:51:35 +0800

    Update Is Subsequence.py

--- Algorithms/392. Is Subsequence/Is Subsequence.py
+++ Algorithms/392. Is Subsequence/Is Subsequence.py
@@ -17,3 +17,17 @@ Output: false
 
 
 '''
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        dp = [[0 for j in range(len(t)+1)] for i in range(len(s)+1)]
+        #dp[i][j]: the max overlap length of s end at [i-1] and t end at [j-1]
+
+        for i in range(1,len(s)+1):
+            for j in range(1,len(t)+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=dp[i][j-1]
+        return True if dp[-1][-1]==len(s) else False
