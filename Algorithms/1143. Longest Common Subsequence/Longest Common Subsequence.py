8bde772902da8e78d727f5a1d6913527ed8851fb
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-20 22:47:46 +0800

    Update Longest Common Subsequence.py

--- Algorithms/1143. Longest Common Subsequence/Longest Common Subsequence.py
+++ Algorithms/1143. Longest Common Subsequence/Longest Common Subsequence.py
@@ -28,3 +28,18 @@ Explanation: There is no such common subsequence, so the result is 0.
 
 
 '''
+
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+        #dp[i][j] means the longest overlap of 0~i-1 of text1 and 0~j-1 of text2
+
+        for i in range(1,len(text1)+1):
+            for j in range(1,len(text2)+1):
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return dp[-1][-1]
