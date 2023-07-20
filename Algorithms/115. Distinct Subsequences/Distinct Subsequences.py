9c9b3e45a514697b9281a459ae21ec820d617c9e
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-20 23:54:39 +0800

    Create Distinct Subsequences.py

---
+++ Algorithms/115. Distinct Subsequences/Distinct Subsequences.py
@@ -0,0 +1,19 @@
+'''
+
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+
+The test cases are generated so that the answer fits on a 32-bit signed integer.
+
+ 
+
+Example 1:
+Input: s = "rabbbit", t = "rabbit"
+Output: 3
+
+
+Example 2:
+Input: s = "babgbag", t = "bag"
+Output: 5
+
+
+'''
