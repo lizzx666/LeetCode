60245bf7ceeadaac266deffafa797dea579f33fa
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-20 23:16:44 +0800

    Update Maximum Subarray.py

--- Algorithms/53. Maximum Subarray/Maximum Subarray.py
+++ Algorithms/53. Maximum Subarray/Maximum Subarray.py
@@ -20,6 +20,19 @@ Output: 23
 
 '''
 
+#DP
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [0 for i in range(len(nums))]
+        #dp[i] means max value of array end at nums[i]
+        dp[0]=nums[0]
+
+        for i in range(1,len(nums)):
+            dp[i]=max(dp[i-1]+nums[i],nums[i])
+        
+        return max(dp)
+
+
 #Greedy Algorithm
 
 class Solution:
