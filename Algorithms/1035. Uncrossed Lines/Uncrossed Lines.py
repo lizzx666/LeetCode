e0c0b19862603c1b4eda5f365e1fcfb38315ee2e
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-20 23:01:06 +0800

    Update Uncrossed Lines.py

--- Algorithms/1035. Uncrossed Lines/Uncrossed Lines.py
+++ Algorithms/1035. Uncrossed Lines/Uncrossed Lines.py
@@ -27,3 +27,18 @@ Input: nums1 = [1,3,7,1,7,5], nums2 = [1,9,2,5,1]
 Output: 2
 
 '''
+
+
+
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[0 for j in range(len(nums2)+1)] for i in range(len(nums1)+1)]
+        #dp[i][j] are longest common overlap of [0,i-1] of nums1 and [0,j-1] of nums2
+        for i in range(1,len(nums1)+1):
+            for j in range(1,len(nums2)+1):
+                if nums1[i-1]==nums2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return dp[-1][-1]
+
