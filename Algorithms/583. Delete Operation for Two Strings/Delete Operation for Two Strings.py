1f2f16bd3a72fc95108cffb089668d2de637d07f
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-21 00:57:29 +0800

    Update Delete Operation for Two Strings.py

--- Algorithms/583. Delete Operation for Two Strings/Delete Operation for Two Strings.py
+++ Algorithms/583. Delete Operation for Two Strings/Delete Operation for Two Strings.py
@@ -18,3 +18,22 @@ Output: 4
 
 
 '''
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp=[[0 for j in range(len(word2)+1)] for i in range(len(word1)+1)]
+        #dp[i][j]: min operation of making word1 end with i-1 equal to word2 end with j-1 to be the same
+        for i in range(len(word1)+1):
+            dp[i][0]=i
+        
+        for j in range(len(word2)+1):
+            dp[0][j]=j
+        
+        for i in range(1,len(word1)+1):
+            for j in range(1,len(word2)+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=min(dp[i-1][j]+1,dp[i][j-1]+1,dp[i-1][j-1]+2)
+        return dp[-1][-1]
