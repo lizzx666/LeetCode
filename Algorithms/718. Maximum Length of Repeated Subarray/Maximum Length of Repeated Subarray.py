8861190520943cbc0ff8e2b8e537e6906ed95d93
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-20 16:28:42 +0800

    Update Maximum Length of Repeated Subarray.py

--- Algorithms/718. Maximum Length of Repeated Subarray/Maximum Length of Repeated Subarray.py
+++ Algorithms/718. Maximum Length of Repeated Subarray/Maximum Length of Repeated Subarray.py
@@ -15,3 +15,20 @@ Explanation: The repeated subarray with maximum length is [0,0,0,0,0].
 
 
 '''
+
+
+
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[0 for j in range(len(nums2)+1)] for i in range(len(nums1)+1)]
+
+        #dp[i][j] means the max length of repeated subarray for i-1 th element in nums1 and j-1th element in nums2
+        res = 0
+        for i in range(1,len(nums1)+1):
+            for j in range(1,len(nums2)+1):
+                if nums1[i-1]==nums2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+
+                if dp[i][j]>res:
+                    res = dp[i][j]
+        return res
