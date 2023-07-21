ba0c13821f647267e2625afb1afc8e590749df2e
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-21 23:46:06 +0800

    Update Longest Palindromic Subsequence.py

--- Algorithms/516. Longest Palindromic Subsequence/Longest Palindromic Subsequence.py
+++ Algorithms/516. Longest Palindromic Subsequence/Longest Palindromic Subsequence.py
@@ -17,3 +17,22 @@ Explanation: One possible longest palindromic subsequence is "bb".
 
 
 '''
+
+
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        dp=[[0 for j in range(len(s))] for i in range(len(s))]
+        #dp[i][j]:the length of longest palindromuc sequence of s start at i and end at j
+
+        for i in range(len(s)):
+            dp[i][i]=1
+        
+        for i in range(len(s)-1,-1,-1):
+            for j in range(i+1,len(s)):
+                if s[i]==s[j]:
+                    dp[i][j]=dp[i+1][j-1]+2
+                else:
+                    dp[i][j]=max(dp[i][j-1],dp[i+1][j])
+
+        return dp[0][-1]
