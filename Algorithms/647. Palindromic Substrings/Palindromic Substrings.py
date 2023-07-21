52d878b1e8fd14525407d2661225a71973aeed7a
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-21 23:12:55 +0800

    Update Palindromic Substrings.py

--- Algorithms/647. Palindromic Substrings/Palindromic Substrings.py
+++ Algorithms/647. Palindromic Substrings/Palindromic Substrings.py
@@ -19,3 +19,22 @@ Output: 6
 Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 
 '''
+
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        dp = [[0 for i in range(len(s))] for j in range(len(s))]
+        #dp[i][j]: whether the s start with i and end at j is a palindrome
+        res = 0
+        for i in range(len(s)-1,-1,-1):
+            for j in range(i,len(s)):
+                if s[i]==s[j]:
+                    if j-i<=1:
+                        dp[i][j]=1
+                        res+=1
+                    else:
+                        if dp[i+1][j-1]:
+                            dp[i][j]=1
+                            res+=1
+        return res
