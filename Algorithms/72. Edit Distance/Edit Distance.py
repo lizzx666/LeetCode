506ead97d8dce6a9adaeb4b1bc18fa67fea67b18
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-21 22:46:40 +0800

    Update Edit Distance.py

--- Algorithms/72. Edit Distance/Edit Distance.py
+++ Algorithms/72. Edit Distance/Edit Distance.py
@@ -31,6 +31,28 @@ exection -> execution (insert 'u')
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[0 for j in range(len(word2)+1)] for i in range(len(word1)+1)]
+
+        #dp[i][j]: min steps to convert word1 end at i-1 to word2 end at j-1
+
+        #if word2 is empty
+        for i in range(len(word1)+1):
+            dp[i][0]=i
+        #if word1 is empty
+        for j in range(len(word2)+1):
+            dp[0][j]=j
+
+        for i in range(1,len(word1)+1):
+            for j in range(1,len(word2)+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=min(dp[i-1][j]+1,dp[i][j-1]+1,dp[i-1][j-1]+1)
+        return dp[-1][-1]
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
         m = len(word1)
         n = len(word2)
         dp = [[float('inf') for _ in range(n+1)] for _ in range (m+1)]
