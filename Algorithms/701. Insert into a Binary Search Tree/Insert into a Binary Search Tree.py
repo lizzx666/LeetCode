3e679e77c0f55b8607f6ec9015dc942d82fa0e9f
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-23 01:17:49 +0800

    Update Insert into a Binary Search Tree.py

--- Algorithms/701. Insert into a Binary Search Tree/Insert into a Binary Search Tree.py
+++ Algorithms/701. Insert into a Binary Search Tree/Insert into a Binary Search Tree.py
@@ -21,3 +21,53 @@ Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
 Output: [4,2,7,1,3,5]
 
 '''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+
+#递归法
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            node = TreeNode(val)
+            return node
+        
+        if root.val>val:
+            root.left = self.insertIntoBST(root.left,val)
+        if root.val<val:
+            root.right = self.insertIntoBST(root.right,val)
+        
+        return root
+
+
+#迭代法
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            node = TreeNode(val)
+            return node
+
+        cur = root
+        parent = root
+
+        while cur:
+            parent = cur
+            if cur.val>val:
+                cur = cur.left
+            else:
+                cur = cur.right
+        
+        node = TreeNode(val)
+        if val < parent.val:
+            parent.left = node
+        else:
+            parent.right = node
+        return root
