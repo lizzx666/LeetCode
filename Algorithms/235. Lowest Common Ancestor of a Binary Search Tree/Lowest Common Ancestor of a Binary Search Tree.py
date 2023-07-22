1a8e412deec764f22e416e92e71ef0611b24a563
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-23 00:47:01 +0800

    Update Lowest Common Ancestor of a Binary Search Tree.py

--- Algorithms/235. Lowest Common Ancestor of a Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree.py
+++ Algorithms/235. Lowest Common Ancestor of a Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree.py
@@ -27,12 +27,41 @@ Output: 2
 
 
 
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+#递归法
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root.val>p.val and root.val>q.val:
+            left = self.lowestCommonAncestor(root.left,p,q)
+            return left
+  
+        elif root.val<p.val and root.val<q.val:
+            right = self.lowestCommonAncestor(root.right,p,q)
+            return right
+        else:
+            return root
+
+#迭代法
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
         while root:
-            if p.val < root.val and q.val < root.val:
+            if root.val>p.val and root.val>q.val:
                 root = root.left
-            elif p.val > root.val and q.val > root.val:
+            elif root.val<p.val and root.val<q.val:
                 root = root.right
             else:
                 return root
