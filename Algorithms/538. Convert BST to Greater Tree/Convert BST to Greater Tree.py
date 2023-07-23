ca976f842f514a8aac86943e2f87fa6a99d04973
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-23 16:45:04 +0800

    Update Convert BST to Greater Tree.py

--- Algorithms/538. Convert BST to Greater Tree/Convert BST to Greater Tree.py
+++ Algorithms/538. Convert BST to Greater Tree/Convert BST to Greater Tree.py
@@ -21,3 +21,28 @@ Output: [1,null,1]
 
 
 '''
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def traversal(self,cur):
+        if cur is None:
+            return        
+        self.traversal(cur.right)
+        cur.val+=self.pre
+        self.pre = cur.val
+        self.traversal(cur.left)
+
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.pre = 0
+        self.traversal(root)
+        return root
