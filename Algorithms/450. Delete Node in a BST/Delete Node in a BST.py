defd47e21ffe8456a565ae61257dfb5fb665d007
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-23 15:10:57 +0800

    Update Delete Node in a BST.py

--- Algorithms/450. Delete Node in a BST/Delete Node in a BST.py
+++ Algorithms/450. Delete Node in a BST/Delete Node in a BST.py
@@ -26,3 +26,36 @@ Output: []
 
 
 '''
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val==key:
+            if root.left is None and root.right is None:
+                return None
+            elif root.left and root.right is None:
+                return root.left
+            elif root.left is None and root.right:
+                return root.right
+            else:
+                cur = root.right
+                while cur.left:
+                    cur = cur.left
+                cur.left = root.left
+                return root.right
+        if root.val>key:
+            root.left = self.deleteNode(root.left,key)
+        if root.val<key:
+            root.right = self.deleteNode(root.right,key)
+        
+        return root
