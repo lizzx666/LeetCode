cf687e0f25a1fd8e588177ceed1e37f7afb3bfd5
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-27 00:16:44 +0800

    Update Letter Combinations of a Phone Number.py

--- Algorithms/17. Letter Combinations of a Phone Number/Letter Combinations of a Phone Number.py
+++ Algorithms/17. Letter Combinations of a Phone Number/Letter Combinations of a Phone Number.py
@@ -23,3 +23,40 @@ Output: ["a","b","c"]
 
 
 '''
+
+
+
+class Solution:
+    def __init__(self):
+        self.letterMap = [
+            "",     # 0
+            "",     # 1
+            "abc",  # 2
+            "def",  # 3
+            "ghi",  # 4
+            "jkl",  # 5
+            "mno",  # 6
+            "pqrs", # 7
+            "tuv",  # 8
+            "wxyz"  # 9
+        ]
+        self.result = []
+        self.s = ""
+
+    def backtracking(self,digits,index,s):
+        #if index == len(digits):
+        if len(self.s)==len(digits):
+            self.result.append(self.s)
+            return
+        digit = int(digits[index])
+        letter = self.letterMap[digit]
+        for i in range(len(letter)):
+            self.s+=letter[i]
+            self.backtracking(digits,index+1,self.s)
+            self.s = self.s[:-1]
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits)==0:
+            return self.result
+        self.backtracking(digits,0,self.s)
+        return self.result
