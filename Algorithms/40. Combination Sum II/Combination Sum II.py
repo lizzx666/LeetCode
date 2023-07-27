054da961e4117443abbcdb25dba1be2544120931
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-28 00:10:54 +0800

    Update Combination Sum II.py

--- Algorithms/40. Combination Sum II/Combination Sum II.py
+++ Algorithms/40. Combination Sum II/Combination Sum II.py
@@ -30,3 +30,39 @@ Output:
 
 
 '''
+
+
+
+
+class Solution:
+    def __init__(self):
+        self.result = []
+        self.path = []
+        self.cur_sum = 0
+        
+    def backtracking(self,candidates,target,cur_sum,start_index,used):
+        if self.cur_sum>target:
+            return
+        if self.cur_sum==target:
+            self.result.append(self.path[:])
+            return
+        
+        for i in range(start_index,len(candidates)):
+            if i>0 and candidates[i]==candidates[i-1] and used[i-1]==0:
+                continue
+            if self.cur_sum + candidates[i]>target:
+                break
+
+            self.path.append(candidates[i])
+            self.cur_sum+=candidates[i]
+            used[i]=1
+            self.backtracking(candidates,target,self.cur_sum,i+1,used)
+            self.path.pop()
+            self.cur_sum-=candidates[i]
+            used[i]=0
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        used = len(candidates)*[0]
+        self.backtracking(candidates,target,self.cur_sum,0,used)
+        return self.result
