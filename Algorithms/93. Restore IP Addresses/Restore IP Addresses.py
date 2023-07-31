c293dea51836594282de4b06f61b541de664eb5d
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-31 22:24:22 +0800

    Update Restore IP Addresses.py

--- Algorithms/93. Restore IP Addresses/Restore IP Addresses.py
+++ Algorithms/93. Restore IP Addresses/Restore IP Addresses.py
@@ -23,3 +23,36 @@ Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
 
 
 '''
+
+
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        cur = []
+        self.backtracking(s,0,cur,res)
+        return res
+    
+    def backtracking(self,s,start_index,cur,res):
+        if start_index==len(s) and len(cur)==4:
+            res.append('.'.join(cur))
+            return
+        if len(cur) > 4:
+            return
+        
+        for i in range(start_index,min(start_index+3,len(s))): #at most 255
+            if self.isvalid(s,start_index,i):
+                sub = s[start_index:i+1]
+                cur.append(sub)
+                print(cur)
+                self.backtracking(s,i+1,cur,res)
+                cur.pop()
+
+    def isvalid(self,s,start,end):
+        if start>end:
+            return False
+        if s[start]=='0' and start!=end: #single 0 is valid but start with 0 is invalid
+            return False
+        num = int(s[start:end+1])
+        return 0<=num<=255
+
