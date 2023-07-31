62699359ca41b49eb16d896361f3ddfa580eecf7
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-08-01 00:06:49 +0800

    Update Permutations.py

--- Algorithms/46. Permutations/Permutations.py
+++ Algorithms/46. Permutations/Permutations.py
@@ -18,3 +18,28 @@ Output: [[1]]
 
 
 '''
+
+
+
+
+class Solution:
+    
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        path = []
+        res = []
+        used = [0]*len(nums)
+        self.backtracking(nums,used,path,res)
+        return res
+    
+    def backtracking(self,nums,used,path,res):
+        if len(path)==len(nums):
+            res.append(path[:])
+            return
+        for i in range(len(nums)):
+            if used[i]==1:
+                continue
+            used[i]=1
+            path.append(nums[i])
+            self.backtracking(nums,used,path,res)
+            used[i]=0
+            path.pop()
