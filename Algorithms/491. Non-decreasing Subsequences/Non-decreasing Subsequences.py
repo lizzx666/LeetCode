fa0fbf2bf4501a082f5adc62cdf36e0a49ad91d0
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-07-31 23:49:06 +0800

    Update Non-decreasing Subsequences.py

--- Algorithms/491. Non-decreasing Subsequences/Non-decreasing Subsequences.py
+++ Algorithms/491. Non-decreasing Subsequences/Non-decreasing Subsequences.py
@@ -15,3 +15,27 @@ Output: [[4,4]]
 
 
 '''
+
+
+
+
+class Solution:
+
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        path = []
+        res = []
+        self.backtracking(nums,0,path,res)
+        return res
+
+    def backtracking(self,nums,start_index,path,res):
+        if len(path)>1:
+            res.append(path[:])
+        
+        uset = set()
+        for i in range(start_index,len(nums)):
+            if (len(path)>0 and path[-1]>nums[i]) or nums[i] in uset:
+                continue
+            uset.add(nums[i])
+            path.append(nums[i])
+            self.backtracking(nums,i+1,path,res)
+            path.pop()
