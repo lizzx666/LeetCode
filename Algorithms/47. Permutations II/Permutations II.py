9fc3fd6dc9ba68cf481ad118b7db9f5b99b69971
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-08-01 23:49:17 +0800

    Update Permutations II.py

--- Algorithms/47. Permutations II/Permutations II.py
+++ Algorithms/47. Permutations II/Permutations II.py
@@ -18,3 +18,28 @@ Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 
 
 '''
+
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        path = []
+        res = []
+        used = [0]*len(nums)
+        nums.sort()
+        self.backtracking(nums,used,path,res)
+        return res
+    
+    def backtracking(self,nums,used,path,res):
+        if len(path)==len(nums):
+            res.append(path[:])
+            return
+        
+        for i in range(len(nums)):
+            if (i>0 and nums[i]==nums[i-1] and used[i-1]==0) or used[i]==1:
+                continue
+            used[i]=1
+            path.append(nums[i])
+            self.backtracking(nums,used,path,res)
+            used[i]=0
+            path.pop()
