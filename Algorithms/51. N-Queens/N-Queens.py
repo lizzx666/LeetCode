daca7bb1ff7596a257e61a5afcd430c552cecbb1
Zhong Xin <43647550+lizzx666@users.noreply.github.com>
2023-08-03 16:49:07 +0800

    Update N-Queens.py

--- Algorithms/51. N-Queens/N-Queens.py
+++ Algorithms/51. N-Queens/N-Queens.py
@@ -21,3 +21,51 @@ Output: [["Q"]]
 
 
 '''
+
+
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        chessboard = ['.' * n for _ in range(n)]
+        self.backtracking(chessboard,n,0,res)
+        return res
+
+    def backtracking(self,chessboard,n,row,res):
+        if row==n:
+            res.append(chessboard[:])
+            return
+        
+        for col in range(n):
+            if self.isvalid(row,col,chessboard):
+                chessboard[row] = chessboard[row][:col]+'Q'+chessboard[row][col+1:]
+                #chessboard[row][i]='Q'
+                self.backtracking(chessboard,n,row+1,res)
+                chessboard[row] = chessboard[row][:col]+'.'+chessboard[row][col+1:]
+                #chessboard[row][i]='.'
+    
+    def isvalid(self,row,col,chessboard):
+        #check whether upside have 'Q
+        for i in range(row):
+            if chessboard[i][col]=='Q':
+                return False
+        
+        #check whether left-up side have 'Q'
+        i = row-1
+        j = col-1
+        while i>=0 and j>=0:
+            if chessboard[i][j]=='Q':
+                return False
+            i-=1
+            j-=1
+        
+        #check whether right-up side have 'Q'
+        i = row-1
+        j = col+1
+        while i>=0 and j<len(chessboard):
+            if chessboard[i][j]=='Q':
+                return False
+            i-=1
+            j+=1
+        return True
